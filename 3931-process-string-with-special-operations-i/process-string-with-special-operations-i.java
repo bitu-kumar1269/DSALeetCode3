21d84a52f30e7ce23c83bb59d3711cfc2687a163
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-06-16 19:38:12 +0530

    Time: 3 ms (100.00%) | Memory: 55.6 MB (28.68%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -1,22 +1,22 @@
 class Solution {
     public String processStr(String s) {
-        int n = s.length();
         StringBuilder str = new StringBuilder();
 
-        for(int i=0; i<n; i++){
-            if(s.charAt(i) == '#'){
-                str.append(str.toString());
-            }
-            else if(s.charAt(i) == '*'){
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '*'){
                 if (str.length() > 0) {
                     str.deleteCharAt(str.length() - 1);
                 }
             }
-            else if(s.charAt(i) == '%'){
+            else if(ch == '#'){
+                str.append(str.toString());
+            }
+            else if(ch == '%'){
                 str.reverse();
             }
             else{
-                str.append(s.charAt(i));
+                str.append(ch);
             }
         }
         return str.toString();

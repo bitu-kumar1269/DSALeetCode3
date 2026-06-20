355bfcc7d0687bce82708f5cbf1a1fb4f904fed4
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-06-20 10:57:32 +0530

    Time: 57 ms (41.18%) | Memory: 123.4 MB (85.29%) - LeetSync

--- 1968-maximum-building-height/maximum-building-height.java
+++ 1968-maximum-building-height/maximum-building-height.java
@@ -1,45 +1,40 @@
 class Solution {
     public int maxBuilding(int n, int[][] restrictions) {
-        List<int[]> list = new ArrayList<>();
-
-        for (int[] r : restrictions) {
-            list.add(new int[]{r[0], r[1]});
+        List<int[]> list=new ArrayList<>();
+        list.add(new int[]{1,0});
+        for(int[] restriction:restrictions){
+            list.add(restriction);
         }
-
-        list.add(new int[]{1, 0});
-        list.sort((a, b) -> Integer.compare(a[0], b[0]));
-
-        if (list.get(list.size() - 1)[0] != n) {
-            list.add(new int[]{n, n - 1});
+        Collections.sort(list,new IDSorter());
+        
+        if(list.get(list.size()-1)[0]!=n){
+            list.add(new int[]{n,n-1});
         }
-
-        int m = list.size();
-
-        // Left -> Right
-        for (int i = 1; i < m; i++) {
-            int dist = list.get(i)[0] - list.get(i - 1)[0];
-            list.get(i)[1] =Math.min(list.get(i)[1],list.get(i - 1)[1] + dist);
-        }
-
-        // Right -> Left
-        for (int i = m - 2; i >= 0; i--) {
-            int dist = list.get(i + 1)[0] - list.get(i)[0];
-            list.get(i)[1] =Math.min(list.get(i)[1],list.get(i + 1)[1] + dist);
-        }
-
-        long ans = 0;
-
-        for (int i = 1; i < m; i++) {
-            long x1 = list.get(i - 1)[0];
-            long h1 = list.get(i - 1)[1];
-
-            long x2 = list.get(i)[0];
-            long h2 = list.get(i)[1];
-
-            long d = x2 - x1;
-            ans = Math.max(ans, (h1 + h2 + d) / 2);
-        }
-
-        return (int) ans;
+            
+       for(int i=1;i<list.size();i++){
+           list.get(i)[1]=Math.min(list.get(i)[1],list.get(i-1)[1] + list.get(i)[0]-list.get(i-1)[0]);
+       }
+        
+       for(int i=list.size()-2;i>=0;i--){
+           list.get(i)[1]=Math.min(list.get(i)[1],list.get(i+1)[1] + list.get(i+1)[0] - list.get(i)[0]);
+       } 
+        
+       int result=0;
+       for(int i=1;i<list.size();i++){
+           int h1=list.get(i-1)[1];  // heigth of previous restriction
+           int h2=list.get(i)[1];   // height of current restriction
+           int x=list.get(i-1)[0]; // id of previous restriction
+           int y=list.get(i)[0];   // id of current restriction
+       
+          result=Math.max(result,Math.max(h1,h2) + (y-x-Math.abs(h1-h2))/2);
+       } 
+        return result;
+    }
+    
+    public class IDSorter implements Comparator<int[]>{
+        @Override
+        public int compare(int[] myself,int[] other){
+            return myself[0]-other[0];
+        } 
     }
 }

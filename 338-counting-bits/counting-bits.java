260b7be5b468d9de8bcdefdc689f7734aebbd7a9
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-06-23 14:01:49 +0530

    Time: 2 ms (96.56%) | Memory: 48.5 MB (84.87%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -3,14 +3,7 @@ class Solution {
         int[] ans = new int[n+1];
 
         for(int i=0; i<=n; i++){
-            String binaryStr = Integer.toBinaryString(i);
-            int count = 0;
-            for(int j=0; j< binaryStr.length(); j++){
-                if(binaryStr.charAt(j) == '1'){
-                    count++;
-                }
-            }
-            ans[i] = count;
+            ans[i] = ans[i >> 1] + (i & 1);
         }
         return ans;
     }

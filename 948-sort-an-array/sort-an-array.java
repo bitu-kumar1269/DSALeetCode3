c2b40a6a96847d8e09160133a4658750c6146324
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-06-24 15:25:28 +0530

    Time: 21 ms (91.40%) | Memory: 83.3 MB (53.31%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,9 +1,7 @@
 class Solution {
     public int[] sortArray(int[] nums) {
         Arrays.sort(nums);
-        for(int i=0; i<nums.length; i++){
-            System.out.print(nums[i]);
-        }
+        
         return nums;
     }
 }

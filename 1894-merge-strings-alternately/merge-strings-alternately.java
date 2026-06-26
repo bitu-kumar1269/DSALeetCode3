cce59dc78ad9bff3ff7f05908b6dadc13cc688e5
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-06-26 10:54:11 +0530

    Time: 1 ms (95.91%) | Memory: 42.7 MB (94.72%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,38 +1,34 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        /*
+        
         StringBuilder str = new StringBuilder();
+        int n = word1.length();
+        int m = word2.length();
 
         int left =0, right=0;
-        while(left < word1.length() && right < word2.length()){
-            str.append(word1.charAt(left));
-            str.append(word2.charAt(right));
-            left++;
-            right++;
+        while(left < n || right < m){
+            if(left < n)
+                str.append(word1.charAt(left));
+                left++;
+            if(right < m)    
+                str.append(word2.charAt(right));
+                right++;
+            
         }
         
-        while(left < word1.length()){
-            str.append(word1.charAt(left));
-            left++;
-        }
-
-        while(right < word2.length()){
-            str.append(word2.charAt(right));
-            right++;
-        }
         return str.toString();
-        */
+        
 
-        StringBuilder str = new StringBuilder();
-        int m = word1.length(), n = word2.length();
-        int maxLen = Math.max(m, n);
+        // StringBuilder str = new StringBuilder();
+        // int m = word1.length(), n = word2.length();
+        // int maxLen = Math.max(m, n);
 
-        for (int i = 0; i < maxLen; i++) {
-            if (i < m) str.append(word1.charAt(i));
-            if (i < n) str.append(word2.charAt(i));
-        }
+        // for (int i = 0; i < maxLen; i++) {
+        //     if (i < m) str.append(word1.charAt(i));
+        //     if (i < n) str.append(word2.charAt(i));
+        // }
 
-        return str.toString();
+        // return str.toString();
         
     }
 }

7c08ace9b072ae8599922da6a2d3403e5d3bce6a
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-07-02 15:08:04 +0530

    Time: 4 ms (99.56%) | Memory: 47.1 MB (92.42%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -2,29 +2,30 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        List<List<Integer>> answer = new ArrayList<>();
+        // Use boolean arrays to keep track of existing numbers (offset by 1000 for negative numbers)
+        boolean[] exist1 = new boolean[2001];
+        boolean[] exist2 = new boolean[2001];
         
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
-        
-        for (int num : nums1) set1.add(num);
-        for (int num : nums2) set2.add(num);
+        for (int num : nums1) {
+            exist1[num + 1000] = true;
+        }
+        for (int num : nums2) {
+            exist2[num + 1000] = true;
+        }
         
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
         
-        for (int num : set1) {
-            if (!set2.contains(num)) {
-                list1.add(num);
+        // Scan through the valid range [-1000, 1000]
+        for (int i = 0; i <= 2000; i++) {
+            if (exist1[i] && !exist2[i]) {
+                list1.add(i - 1000); // Revert the offset
+            } else if (exist2[i] && !exist1[i]) {
+                list2.add(i - 1000);
             }
         }
         
-        for (int num : set2) {
-            if (!set1.contains(num)) {
-                list2.add(num);
-            }
-        }
-        
+        List<List<Integer>> answer = new ArrayList<>();
         answer.add(list1);
         answer.add(list2);
         
